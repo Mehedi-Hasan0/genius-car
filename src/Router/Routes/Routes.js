342f28af694ca919ebbe9aef9cc63546ffb9d6a9
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-02 20:41:16 +0600

    orders section done

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,7 +1,9 @@
 import Main from "../../Layout/Main";
+import Checkout from "../../Pages/Checkout/Checkout";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
+import Orders from "../../Pages/Orders/Orders";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -22,6 +24,16 @@ const router = createBrowserRouter([
                 path: '/signup',
                 element: <SignUp></SignUp>
             },
+            {
+                path: '/checkout/:id',
+                element: <Checkout></Checkout>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/orders',
+                element: <Orders></Orders>,
+                loader: ({ params }) => fetch(`http://localhost:5000/orders/${params.id}`)
+            }
         ]
     }
 ]);
